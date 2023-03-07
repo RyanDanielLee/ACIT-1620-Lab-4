92086bf90ddd2a2308af4f5f3b6bcc7f0c10ea34
Ryan <ryan.lee8867@outlook.com>
2023-03-06 16:10:47 -0800

    Added course page functionality that implements course lookup, and validates user input

---
+++ Script/script.js
@@ -0,0 +1,49 @@
+let class1 = {
+    code: "ACIT 1620", 
+    name: "Web Fundamental Technologies"
+};
+
+let class2 = {
+    code: "ACIT 1630", 
+    name: "Database Systems"
+};
+
+let class3 = {
+    code: "ACIT 1420", 
+    name: "Introduction to System Administrators"
+};
+
+const courseList = [class1, class2, class3];
+
+let userInput;
+let continueLoop = true;
+
+while (continueLoop) {
+    userInput = prompt("Please enter a 4-digit code:");
+    
+    if (userInput.length == 4 && isNaN(userInput) == false) {
+        continueLoop = false;
+        //console.log("This works");
+    }
+    else {
+        console.log("Error: Please enter a valid 4-digit number:");
+    }
+}
+
+let isInputInList = false;
+
+for (let i = 0; i < courseList.length; i++) {
+    if(courseList[i].code.includes(userInput)) {
+        console.log(`Yes I am taking the course: ${courseList[i].code}`);
+        isInputInList = true;
+    }
+}
+
+if (isInputInList == false) {
+    let userClass = {
+        code: userInput,
+        name: null,
+    };
+
+    courseList.push(userClass);
+}
